e46317a2d26edadd96a34e4cd47b60d15da42312
alisson-moura <alisson.mo.moura@outlook.com.br>
2022-07-12 10:44:08 -0300

    estilização do layout do header

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
-
-import Button from './components/Button'
 import { GlobalStyle } from './styles/globlal'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button />
-
+      <Router />
       <GlobalStyle />
     </ThemeProvider>
   )
---
+++ src/Router.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { History } from './pages/History'
+import { DefaultLayout } from './layouts/Default'
+
+export function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<DefaultLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/history" element={<History />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
