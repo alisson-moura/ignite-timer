099ca61557c34fbc11c9261a57bf0a013b87d35e
alisson-moura <alisson.mo.moura@outlook.com.br>
2022-07-14 10:05:49 -0300

    Adicionado hookform e zod para validação de inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,6 @@
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
 import { Play } from 'phosphor-react'
 import {
   CountdownContainer,
@@ -9,10 +12,33 @@ import {
   TaskInput,
 } from './styles'
 
+const formValidationSchema = zod.object({
+  task: zod.string().min(2, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'A tarefa precisa de no minímo 5 minutos.')
+    .max(60, 'A tarefa precisa levar no maxímo 60 minutos.'),
+})
+
+type IFormData = zod.infer<typeof formValidationSchema>
+
 export function Home() {
+  const { register, handleSubmit, watch, reset } = useForm<IFormData>({
+    resolver: zodResolver(formValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 5,
+    },
+  })
+
+  const onSubmit = (data: IFormData) => reset()
+
+  const taskInput = watch('task')
+  const isSubmitDisabled = !taskInput
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(onSubmit)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
@@ -20,6 +46,7 @@ export function Home() {
             id="text"
             list="task-suggestion"
             placeholder="Dê um nome para o seu projeto"
+            {...register('task')}
           />
           <datalist id="task-suggestion">
             <option value="Projeto 1" />
@@ -36,6 +63,7 @@ export function Home() {
             max={60}
             id="minutesAmount"
             placeholder="00"
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
           <span>minutos.</span>
         </FormContainer>
@@ -47,7 +75,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartButton type="submit">
+        <StartButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Começar
         </StartButton>
